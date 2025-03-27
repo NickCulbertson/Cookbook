62862e5723c2c4a102f5367b2a48669d7f0713a7
NickCulbertson <nickculbertson@gmail.com>
2025-03-27 09:35:22 -0400

    Update Mac Sample Rate

--- Cookbook/Cookbook/CookbookApp.swift
+++ Cookbook/Cookbook/CookbookApp.swift
@@ -12,7 +12,6 @@ struct CookbookApp: App {
             do {
                 Settings.bufferLength = .short
 
-                // Settings.sampleRate default is 44_100
                 if #available(iOS 18.0, *) {
                     if !ProcessInfo.processInfo.isMacCatalystApp && !ProcessInfo.processInfo.isiOSAppOnMac {
                         // Set samplerRate for iOS 18 and newer
@@ -20,8 +19,8 @@ struct CookbookApp: App {
                     }
                 }
                 if #available(macOS 15.0, *) {
-                    // Set samplerRate for macOS 15 and newer
-                    Settings.sampleRate = 48_000
+                    // Set samplerRate for macOS 15 and newer (reverted back to 44_100)
+                    Settings.sampleRate = 44_100
                 }
 
                 try AVAudioSession.sharedInstance().setPreferredIOBufferDuration(Settings.bufferLength.duration)
